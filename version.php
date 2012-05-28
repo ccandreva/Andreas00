bcc85acdecc83055d5651c8c3480ab45d688f32a
chris <chris@westnet.com>
2012-05-27 23:25:22 -0400

    Update description.

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 $themeversion['name'] 		= 'Andreas00';
 $themeversion['displayname'] 	= __('Andreas00');
 $themeversion['version'] 	= '3.0';
-$themeversion['description'] 	= __('andreas00 template');
+$themeversion['description'] 	= __('Based on the theme Andreas08 by Andreas Viklund');
 $themeversion['author'] 	= 'Mark West, Andreas Viklund';
 $themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.andreasviklund.com';
 $themeversion['admin'] 		= 0;
